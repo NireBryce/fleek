99ec05c3edf0d76e84a567904b000eec09eaf8f5
Nire Bryce <nire@computernope.net>
2024-01-17 20:37:14 -0500

    fleek: apply

--- aliases.nix
+++ aliases.nix
@@ -9,6 +9,8 @@
     
     "fleeks" = "cd ~/.local/share/fleek";
     
+    "gitssh" = "export GIT_SSH=/usr/bin/ssh";
+    
     "latest-fleek-version" = "nix run https://getfleek.dev/latest.tar.gz -- version";
     
     "update-fleek" = "nix run https://getfleek.dev/latest.tar.gz -- update";
--- nire-lysithea/custom.nix
+++ nire-lysithea/custom.nix
@@ -2,7 +2,28 @@
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
 	home.sessionVariables = { 
 		GIT_SSH = "/usr/bin/ssh";
-		EDITOR="micro";
+		EDITOR = "micro";
 	};
+
+	programs.micro = {
+		enable = true;
+		settings = {
+			autoclose = false;
+			autosu = true;
+			cursorline  = true;
+			colorscheme = "dukeubuntu-tc";
+			difgutter = true;
+			eofnewline = true;
+			fastdirty = true;
+			ignorecase = false;
+			keyenu = true;
+			mkparents = true;
+			pluginchannels = "https://raw.githubusercontent.com/micro-editor/plugin-channel/master/channel.json";
+			savehistory = false;
+			tabsize = 2;
+			tsbstospaces = true;
+		};
+	};
+	
  
 }
