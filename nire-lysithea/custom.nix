c1a52a202ede79809947c88d7b80e53a6ca2de4e
Nire Bryce <nire@computernope.net>
2024-01-19 19:00:05 -0500

    ???

--- nire-lysithea/custom.nix
+++ nire-lysithea/custom.nix
@@ -151,7 +151,7 @@
 			# disabled after the fact:
 				# setopt extendedglob                                             # Extended globbing. Allows using regular expressions with *
 				zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'   # Case insensitive tab completion
-				zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"         # Colored completion (different colors for dirs/files/etc)
+				zstyle ':completion:*' list-colors ''${(s.:.)LS_COLORS}         # Colored completion (different colors for dirs/files/etc)
 				zstyle ':completion:*' rehash true                              # automatically find new executables in path 
 			# Speed up completions
 				zstyle ':completion:*' accept-exact '*(N)'
