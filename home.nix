86ac82a1172831566696361fd4461b5404f20049
Nire Bryce <nire@computernope.net>
2024-01-19 23:39:29 -0500

    fleek: apply

--- home.nix
+++ home.nix
@@ -20,6 +20,7 @@
   # programs are installed and configuration applied to dotfiles
   home.packages = [
     # user selected packages
+    pkgs.topgrade
     pkgs.asdf-vm
     pkgs.glfw
     pkgs.atuin
