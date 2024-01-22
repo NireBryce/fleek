0ac2e8689013a50e40bcc30c9265bbd984c3f6a2
Nire Bryce <nire@computernope.net>
2024-01-22 07:28:18 -0500

    join new system

--- flake.nix
+++ flake.nix
@@ -78,6 +78,32 @@
         ];
       };
       
+      "elly@nire-galatea" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        modules = [
+          ./home.nix 
+          ./path.nix
+          ./shell.nix
+          ./user.nix
+          ./aliases.nix
+          ./programs.nix
+          # Host Specific configs
+          ./nire-galatea/elly.nix
+          ./nire-galatea/custom.nix
+          # self-manage fleek
+          {
+            home.packages = [
+              fleek.packages.x86_64-linux.default
+            ];
+          }
+          ({
+           nixpkgs.overlays = [];
+          })
+
+        ];
+      };
+      
     };
   };
 }
