920dff939282bd1bd22aca383c7c7b8d6ffa0928
Nire Bryce <nire@computernope.net>
2024-02-27 00:49:04 -0500

    fleek: apply

--- .history/nire-durandal/custom_20240227004841.nix
+++ .history/nire-durandal/custom_20240227004841.nix
@@ -1,6 +1,6 @@
 { config, pkgs, misc, ... }: {
 	# FEEL FREE TO EDIT: This file is NOT managed by fleek.
-	imports = [
+	modules = [
 		# ../_specialized/_gaming
     # ../_specialized/_graphical
 		# ../_specialized/_wm/_kde.nix
