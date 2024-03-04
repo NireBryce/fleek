c58491b81aee3be788d5f8f49823fafb070e8ddb
Nire Bryce <nire@computernope.net>
2024-03-03 19:55:44 -0500

    fleek: new system

--- nire-durandal/custom.nix
+++ nire-durandal/custom.nix
@@ -1,13 +1,5 @@
-{ config, pkgs, misc, ... }: {
-	# FEEL FREE TO EDIT: This file is NOT managed by fleek.
-	imports = [
-		# ../_specialized/_gaming
-    # ../_specialized/_graphical
-		# ../_specialized/_wm/_kde.nix
-    # ../_specialized/_sound
-    # ../_specialized/_dev
-    # ./_packages.nix
-	];
+{ pkgs, misc, ... }: {
+  # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
+
+ 
 }
-
-
