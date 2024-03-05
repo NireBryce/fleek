7950b54318959595243369d09bec844b00ecbd5c
Nire Bryce <nire@computernope.net>
2024-03-05 18:23:41 -0500

    fleek: apply

--- _dotfiles/_linking.nix
+++ _dotfiles/_linking.nix
@@ -1,5 +1,11 @@
 {...}:
 
 {
-  home.file.".config/zsh-powerlevel10k/.p10k.zsh".source = config/zsh-powerlevel10k/.p10k.zsh;
+  home.file.".config/zsh-powerlevel10k/.p10k.zsh".source = "config/zsh-powerlevel10k/.p10k.zsh";
+  home.file.".gitconfig".source = "config/.gitconfig";
+  home.file.".profile".source = "config/.profile";
+  home.file.".bashrc".source = "config/.bashrc";
+  home.file.".bash_profile".source = "config/.bash_profile";
+  home.file.".bash_logout".source = "config/.bash_logout";
+  
 }
