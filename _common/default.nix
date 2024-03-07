21b492730da228d1c58bc58b1e019e6f194f0b68
Nire Bryce <nire@computernope.net>
2024-03-07 18:41:38 -0500

    fleek: apply

--- _common/default.nix
+++ _common/default.nix
@@ -2,7 +2,7 @@
 {
   imports = [
   ./_packages.nix
-  ./_managed-configs
+  # ./_managed-configs/_micro.nix
   ./_services.nix
   # ./_sh.nix # dunno what this does, it's extracted from fleek
   ];
--- _dotfiles/_linking.nix
+++ _dotfiles/_linking.nix
@@ -1,9 +1,21 @@
 {...}:
 
-{
-  home.file."./.profile".source = config/.profile;
-  home.file."./.bashrc".source = config/.bashrc;
-  home.file."./.bash_profile".source = config/.bash_profile;
-  home.file."./.bash_logout".source = config/.bash_logout;
+{ # TODO: split this out into where they're installed.
+  home.file = {
+  "./.profile".source = config/.profile;
+  "./.bashrc".source = config/.bashrc;
+  "./.bash_profile".source = config/.bash_profile;
+  "./.bash_logout".source = config/.bash_logout;
+  "./.config/zellij".source = config/zellij;
+  "./.config/zsh".source = config/zsh;
+  "./.config/atuin".source = config/atuin;
+  "./.config/micro".source = config/micro;
+  "./.config/kitty".source = config/kitty;
+  "./.config/promnesia".source = config/promnesia;
+  "./.config/steamtinkerlaunch".source = config/steamtinkerlaunch;
+  "./config/sway".source = config/sway;
+  # "./zsh-abbr".source = config/zsh-abbr;
+  "./.config/zsh.d".source = config/zsh.d;
+  };
 
 }
