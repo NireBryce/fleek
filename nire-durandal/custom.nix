2193b7cdfd55f3e7df1d690ebaba26f457a4ff43
Nire Bryce <nire@computernope.net>
2024-03-23 22:35:00 -0400

    fleek: apply

--- nire-durandal/custom.nix
+++ nire-durandal/custom.nix
@@ -1,14 +1,15 @@
-{ pkgs, misc, ... }: {
+{
 	# FEEL FREE TO EDIT: This file is NOT managed by fleek.
 	imports = [
 		# We split off configs that we might have to remove from specific machines
 		# TODO: clean this up later
-		../_specialized/_gaming
-    ../_specialized/_graphical
-		../_specialized/_wm/_kde.nix
-    ../_specialized/_sound
-    ../_specialized/_dev
+		../_common
 		../_dotfiles
+		../_specialized/_gaming
+		../_specialized/_wm/_kde.nix
+    ../_specialized/_dev
+    ../_specialized/_graphical
+    ../_specialized/_sound
 
 		# Enables middle mouse scrolling instead of paste in kde
 		./_bugfixes/_middle-mouse-scroll.nix
--- nire-lysithea/custom.nix
+++ nire-lysithea/custom.nix
@@ -1,11 +1,11 @@
-{ lib, config, pkgs, misc, ... }: 
 {
 	# FEEL FREE TO EDIT: This file is NOT managed by fleek.
 	imports = [
 		../_common
+		../_dotfiles
+		../_specialized/_wm/_kde.nix
     ../_specialized/_dev
     ../_specialized/_graphical
-		../_specialized/_wm/_kde.nix
     ../_specialized/_sound
 	];
 }
